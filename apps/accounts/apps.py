b4af85b1a2d06a702625097a504e9f4b2aa1d38d
hardikKanhasoft <hardikjethava.kanhasoft@gmail.com>
2023-06-13 18:59:14 +0530

    apps and some changes

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JwtAuthConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jwt_auth'
+    name = 'accounts'
